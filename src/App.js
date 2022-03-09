801152e17aab87252986fef33497c2aa2ed08413
wangxu0828 <2245267509@qq.com>
2022-03-09 17:58:15 +0800

    234

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-import React from 'react';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom'
+
+import Layout from './pages/Layout'
+import Login from './pages/Login'
+import NotFount from './pages/NotFount'
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Switch>
+        <Redirect exact from="/" to="/home"></Redirect>
+        <Route path="/login" component={Login}></Route>
+        <Route path="/home" component={Layout}></Route>
+        <Route component={NotFount}></Route>
+      </Switch>
+    </Router>
+  )
 }
